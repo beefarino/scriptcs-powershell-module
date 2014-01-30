13c6bfb001f659cd91fab1062ba9fc231036166e
beefarino <jimchristopher@gmail.com>
2014-01-30 16:45:58 -0500

    rounded out unit tests for session cmdlets

--- src/CodeOwls.PowerShell.ScriptCS.Tests/ScriptCSSessions.cs
+++ src/CodeOwls.PowerShell.ScriptCS.Tests/ScriptCSSessions.cs
@@ -40,5 +40,66 @@ namespace CodeOwls.PowerShell.ScriptCS.Tests
             Assert.Empty(results);
         }
 
+        [Fact]
+        public void CanRemoveExistingSession()
+        {
+            var results = Invoke(new[]{
+                "new-scriptcssession -session '4asdf' | out-null" ,
+                "get-scriptcssession -session '4asdf'" ,
+            });
+
+            Assert.Equal(1, results.Count);
+            results = Invoke(new[]{
+                "remove-scriptcssession -session 4asdf -force",
+            });
+
+            Assert.Equal(0, results.Count);
+        }
+
+        [Fact]
+        public void CanRemoveNonexistingSessionWithoutError()
+        {
+            var results = Invoke(new[]{
+                "get-scriptcssession -session 'doesnotexist'" ,
+            });
+
+            // no error
+        }
+
+        [Fact]
+        public void GetScriptCSSessionFetchesExistingSessions()
+        {
+            var results = Invoke(new[]{
+                "new-scriptcssession -session '1asdf' | out-null" ,
+                "get-scriptcssession",
+            });
+
+            Assert.NotNull(results);
+            Assert.Equal(1, results.NotNull().Count());            
+        }
+
+        [Fact]
+        public void GetScriptCSSessionFetchesExistingSessionsByName()
+        {
+            var results = Invoke(new[]{
+                "new-scriptcssession -session '2asdf' | out-null" ,
+                "get-scriptcssession -session '2asdf'",
+            });
+
+            Assert.NotNull(results);
+            Assert.Equal(1, results.NotNull().Count());
+        }
+
+        [Fact]
+        public void GetScriptCSSessionDoesNotFetchNonExistingSessions()
+        {
+            var results = Invoke(new[]{
+                "new-scriptcssession -session '3asdf' | out-null" ,
+                "get-scriptcssession -session 'Nasdf'",
+            });
+
+            Assert.Equal(0, results.Count);
+        }
+
     }
 }
